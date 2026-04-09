dd0d1f5cf171d149d644857a69735e034b70fc83
Cailin Paris <cailinparis123@gmail.com>
2026-04-09 14:28:28 -0400

    Added More Coins and CoinUI

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
     public AudioClip coinClip;
+    private TextMeshProUGUI coinText;
+    public int coinsToGive = 1;
+
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Player player = collision.gameObject.GetComponent<Player>();
-            player.coins += 1;
+            player.coins += coinsToGive;
             player.PlaySFX(coinClip, 0.2f);
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
         }
     }
